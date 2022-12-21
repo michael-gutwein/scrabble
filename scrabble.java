c1e34de8c2e2badade853d43002a94ed8f98c39c
Michael Gutwein <114095949+michael-gutwein@users.noreply.github.com>
2022-12-21 13:22:52 -0500

    WE ARE ALMOST
    DONE

--- scrabble.java
+++ scrabble.java
@@ -1,8 +1,10 @@
 import java.util.Scanner; //main
  
 class scrabble{
+  
  public static int[] getInput(){
         Scanner input = new Scanner(System.in);
+
         System.out.println("Enter row(left side):");
         int placeX = input.nextInt();
         System.out.println("Enter column(top):");
@@ -23,35 +25,37 @@ Scanner input = new Scanner(System.in);
         board g = new board();
        int count = input.nextInt();
        System.out.println(count);
-       if(count == 2){ 
+       if(count == 2){
         player a = new player (0, "a");
         player b = new player (0, "b");
-        
-        
-       
        }
        else if(count == 3) {
         player a = new player (0, "a");
         player b = new player (0, "b");
         player c = new player (0, "c");
-       
        }
        else if(count == 4) {
         player a = new player (0, "a");
         player b = new player (0, "b");
         player c = new player (0, "c");
         player d = new player (0, "d");
-       
        }
-       
        else{
          throw new IllegalArgumentException();
        }
+       boolean gamestart; 
+       while(gamestart = true){
+         
+         
+         
+         
+         
+                 
+       }
        
+       g.printboard();
        
-       
-       
-        g.printboard();
+
         
         
 }

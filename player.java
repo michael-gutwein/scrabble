2a3885a547bf2da2481277a305ce248c932275d4
Michael Gutwein <114095949+michael-gutwein@users.noreply.github.com>
2022-12-22 14:46:24 -0500

    the
    eagbfdrhbgvrsjgvrws ngvnvkjsbnsgvnKJ

--- player.java
+++ player.java
@@ -7,54 +7,105 @@ class player{
  private String type;
  static ArrayList<lettertile> bag;
  
- static { //replace "bag" with "dictionary"
-   HashMap<String, Integer> letterbag = new HashMap<String, Integer>();
-
-letterbag.put("a", 9);
-letterbag.put("b", 2);
-letterbag.put("c", 2);
-letterbag.put("d", 4);
-letterbag.put("e", 12);
-letterbag.put("f", 2);
-letterbag.put("g", 3);
-letterbag.put("h", 2);
-letterbag.put("i", 9);
-letterbag.put("j", 1);
-letterbag.put("k", 1);
-letterbag.put("l", 4);
-letterbag.put("m", 2);
-letterbag.put("n", 6);
-letterbag.put("o", 8);
-letterbag.put("p", 2);
-letterbag.put("q", 1);
-letterbag.put("r", 6);
-letterbag.put("s", 4);
-letterbag.put("t", 6);
-letterbag.put("u", 4);
-letterbag.put("v", 2);
-letterbag.put("w", 2);
-letterbag.put("x", 1);
-letterbag.put("y", 2);
-letterbag.put("z", 1);
-letterbag.put("blank", 2);
- }
-   
- 
- 
- public player(int score, String name){
+  public player(int score, String name){
     this.score = score;
     this.name = name;
     
       
  }
  
- static void draw(){
+
+ 
+ 
+ 
+ 
+ static { //replace "bag" with "dictionary"
+   HashMap<String, Integer> letterdictionary = new HashMap<String, Integer>();
+
+//letter dictionary   
+letterdictionary.put("a", 9);
+letterdictionary.put("b", 2);
+letterdictionary.put("c", 2);
+letterdictionary.put("d", 4);
+letterdictionary.put("e", 12);
+letterdictionary.put("f", 2);
+letterdictionary.put("g", 3);
+letterdictionary.put("h", 2);
+letterdictionary.put("i", 9);
+letterdictionary.put("j", 1);
+letterdictionary.put("k", 1);
+letterdictionary.put("l", 4);
+letterdictionary.put("m", 2);
+letterdictionary.put("n", 6);
+letterdictionary.put("o", 8);
+letterdictionary.put("p", 2);
+letterdictionary.put("q", 1);
+letterdictionary.put("r", 6);
+letterdictionary.put("s", 4);
+letterdictionary.put("t", 6);
+letterdictionary.put("u", 4);
+letterdictionary.put("v", 2);
+letterdictionary.put("w", 2);
+letterdictionary.put("x", 1);
+letterdictionary.put("y", 2);
+letterdictionary.put("z", 1);
+letterdictionary.put("blank", 2);
+
+HashMap<String, Integer> letterValue = new HashMap<String, Integer>();
+//letter values
+letterValue.put("a", 1);
+letterValue.put("b", 3);
+letterValue.put("c", 3);
+letterValue.put("d", 2);
+letterValue.put("e", 1);
+letterValue.put("f", 4);
+letterValue.put("g", 2);
+letterValue.put("h", 4);
+letterValue.put("i", 1);
+letterValue.put("j", 8);
+letterValue.put("k", 5);
+letterValue.put("l", 111);
+letterValue.put("m", 2);
+letterValue.put("n", 6);
+letterValue.put("o", 8);
+letterValue.put("p", 2);
+letterValue.put("q", 1);
+letterValue.put("r", 6);
+letterValue.put("s", 4);
+letterValue.put("t", 6);
+letterValue.put("u", 4);
+letterValue.put("v", 2);
+letterValue.put("w", 2);
+letterValue.put("x", 1);
+letterValue.put("y", 2);
+letterValue.put("z", 1);
+letterValue.put("blank", 2);
+
+ArrayList<lettertile> letterbag = new ArrayList<lettertile>();
+
+for(String s : letterdictionary.keySet()){
+ int n = letterdictionary.get(s);
+ for(String ss : letterValue.keySet()){
+ int m = letterValue.get(ss);
+ 
+ for (int i = 0; i < n; i ++){
+   lettertile qtile = new lettertile(s, m);
+
    
+ }
+ }
+ }
+                            }
+
+ 
    
-   
-  int random = 22 + (int)(Math.random() * 27);
-  System.out.println(random);
-   
+ 
+ 
+
+ 
+ static void draw(){ 
+  int random = (int)(Math.random() * 99);
+  
  }
  
  
@@ -62,46 +113,7 @@ letterbag.put("blank", 2);
  
  
  
-//bag 
+//values
 public static void main(String[] args) {
-HashMap<String, Integer> letterbag = new HashMap<String, Integer>();
-
-letterbag.put("a", 9);
-letterbag.put("b", 2);
-letterbag.put("c", 2);
-letterbag.put("d", 4);
-letterbag.put("e", 12);
-letterbag.put("f", 2);
-letterbag.put("g", 3);
-letterbag.put("h", 2);
-letterbag.put("i", 9);
-letterbag.put("j", 1);
-letterbag.put("k", 1);
-letterbag.put("l", 4);
-letterbag.put("m", 2);
-letterbag.put("n", 6);
-letterbag.put("o", 8);
-letterbag.put("p", 2);
-letterbag.put("q", 1);
-letterbag.put("r", 6);
-letterbag.put("s", 4);
-letterbag.put("t", 6);
-letterbag.put("u", 4);
-letterbag.put("v", 2);
-letterbag.put("w", 2);
-letterbag.put("x", 1);
-letterbag.put("y", 2);
-letterbag.put("z", 1);
-letterbag.put("blank", 2);
-
-System.out.println(letterbag.get("blank") - 1);
-System.out.println();
 }
- 
-  
-  
-  
-  
-  
-  
 }
--- scrabble.java
+++ scrabble.java
@@ -12,7 +12,7 @@ class scrabble{
         int[] inputArray = {placeX, placeY};
         return inputArray;
  }
- 
+  
  
         
 
